754b1f1087303148bd632fdf3b3e5bb00b99e486
denwolf4ever <denwolf4ever@gmail.com>
2024-04-11 13:30:55 +0300

    first commit

---
+++ HW_15.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+void FindOddNumbers(bool IsOdd, int Limit)
+{
+	for (int i = IsOdd; i <= Limit; i+=2)
+	{
+		cout << i << endl;
+	}
+}
+
+int main()
+{
+	bool IsOdd;
+	const int Limit = 15;
+	cout << "If you want to find odd numbers press 0, else 1" << endl;
+	cin >> IsOdd;
+	cout << '\n';
+	FindOddNumbers(IsOdd, Limit);
+	
+}
+
